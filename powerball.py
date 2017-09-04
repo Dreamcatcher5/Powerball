b1f8b9afbe9580597617d1f98d060aca74623709
Dreamcatcher5 <johnstone.ben71@gmail.com>
2017-09-03 23:16:33 -0400

    Started to incorporate drawing into main file

--- powerball.py
+++ powerball.py
@@ -1,89 +1,70 @@
+"""Author: Ben Johnstone"""
+
 import argparse
+from datetime import datetime
 
+from drawing import Drawing
 
-MAX_WHITE = 69
-MAX_RED = 26
-def ParseNumbers():
+#TODO
+# 1 Do argparse for main
+# 2 Write drawing class
+# 3 Figure out what statistics should be reported
+# 4 set up logger
 
-    whiteList = {}
-    redList = {}
-    lrpr = {}
-    wc = 0
-    rc = 0
+def ParseDrawingsFile():
+    """
+    This function reads the input file and returns a list of drawings
 
-    for i in range(1,MAX_WHITE+1):
-        whiteList[i] = 0.0;
-    lrp = {}
-    for i in range(1,MAX_WHITE+1):
-        lrp[i] = 0;
-
-
-
-    for i in range(1,MAX_RED+1):
-        redList[i] = 0.0;
-
-
-    for i in range(1,MAX_RED+1):
-        lrpr[i] = 0;
-    iw = 1
-    ir = 1
+    """
+    drawings = []
     f = open("powerball.txt")
     for line in f:
-        balls = line.split()
-        #print balls
-        # Do whites
-        for b in range(1,6):
-            whiteList[int(balls[b])] += 1
-            wc += 1
-            if lrp[int(balls[b])] == 0:
-                lrp[int(balls[b])] = iw # balls with highest i are last recently picked
-            iw += 1
-        # Do red ball
-        if  not int(balls[6]) > MAX_RED:
-            redList[int(balls[6])] += 1
-            if lrpr[int(balls[6])] == 0:
-                lrpr[int(balls[6])] = ir # balls with highest i are last recently picked
-            ir += 1
-            rc += 1
+        print(line)
+        lsplit = line.split()
+        drawings.append(Drawing(lsplit[0], set(lsplit[1:6]), lsplit[6])
+    f.close()
+    return drawings
 
-# Compile everything, print results
-white_sorted = whiteList.keys()
-for i in range(60,70):
-    whiteList[i] *= 63.3
-white_sorted.sort(cmp=lambda x,y: cmp(whiteList[x], whiteList[y]))
-print
-print "White Balls:"
-for i in white_sorted:
-    print "Ball %d: %f" % (i, whiteList[i]*100/wc)
-    
-white_sorted = lrp.keys()
-white_sorted.sort(cmp=lambda x,y: -1*cmp(lrp[x], lrp[y]))
-print
-print "Least Recent White Balls:"
-for i in white_sorted:
-    print "Ball %d: %f" % (i, lrp[i])
+def PrintStats(whiteList, redList):
+    # Compile everything, print results
+    for i in range(60,70):
+        whiteList[i] *= 63.3
+    white_sorted = sorted(whiteList.keys(), reverse=True)
+    #white_sorted.sort(cmp=lambda x,y: cmp(whiteList[x], whiteList[y]))
+    print()
+    print("White Balls:")
+    for i in white_sorted:
+        print("Ball %d: %f" % (i, whiteList[i]*100/wc))
+        
+    white_sorted = sorted(lrp.keys(), reverse=True)
+    #white_sorted.sort(cmp=lambda x,y: -1*cmp(lrp[x], lrp[y]))
+    print()
+    print("Least Recent White Balls:")
+    for i in white_sorted:
+        print("Ball %d: %f" % (i, lrp[i]))
 
-#Red balls
-red_sorted = redList.keys()
+    #Red balls
+    red_sorted = sorted(redList.keys(), reverse=True)
+    #red_sorted.sort(cmp=lambda x,y: -cmp(redList[x], redList[y]))
+    red_sorted
+    print()
+    print("Red Balls:")
+    for i in red_sorted:
+        print("Ball %d: %f" % (i, redList[i]*100/rc))
+        
 
-red_sorted.sort(cmp=lambda x,y: -cmp(redList[x], redList[y]))
-print
-print "Red Balls:"
-for i in red_sorted:
-    print "Ball %d: %f" % (i, redList[i]*100/rc)
-    
-
-red_sorted = lrpr.keys()
-red_sorted.sort(cmp=lambda x,y: -1*cmp(lrpr[x], lrpr[y]))
-print
-print "Least Recent Red Balls:"
-for i in red_sorted:
-    print "Ball %d: %f" % (i, lrpr[i])
+    red_sorted = lrpr.keys()
+    red_sorted.sort(cmp=lambda x,y: -1*cmp(lrpr[x], lrpr[y]))
+    print()
+    print("Least Recent Red Balls:")
+    for i in red_sorted:
+        print ("Ball %d: %f" % (i, lrpr[i]))
 
 
 def Main():
 
-    whiteList, redList = ParseNumbers()
+    drawings = []
+    whiteList, redList = ParseDrawingsFile()
     PrintStats(whiteList, redList)
 
 if __name__ == "__main__":
