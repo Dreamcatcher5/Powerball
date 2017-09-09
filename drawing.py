b427628035ea47409cee1b9cf3466e7c12bfe0f5
Dreamcatcher5 <johnstone.ben71@gmail.com>
2017-09-09 17:12:35 -0400

    Implemented WinAmount, fixed declaration errors

--- drawing.py
+++ drawing.py
@@ -1,4 +1,7 @@
 """Author: Ben Johnstone"""
+
+from datetime import datetime
+
 MAX_WHITE = 69
 MAX_RED = 26
 
@@ -6,20 +9,20 @@ class Drawing():
 
 
 	NUM_WHITES = 5
+	JACKPOT = -1
 
 	def __init__(self, date, numbers, powerball):
-		self._date = date
+		self._date = datetime.strptime(date, "%m/%d/%Y")
 		self._numbers = numbers
 		self._powerball = powerball
 
-	def GetDate():
+	def GetDate(self):
 		return self._date
 
-	def GetNumbers():
+	def GetNumbers(self):
 		return self._numbers
 
-
-	def GetPowerball():
+	def GetPowerball(self):
 		return self._powerball
 
 	def MatchingNumbers(self, picks):
@@ -34,5 +37,24 @@ class Drawing():
 
 	def WinAmount(self, picks):
 		"""Determine winnings based on the picks"""
+		amt = 0
+		red = MatchingPowerball(self, picks)
+		numMatching = MatchingNumbers(self, picks)
 
+		if not red and numMatching < 3:
+			amt =  0
+		elif red and numMatching == self.NUM_WHITES:
+			amt = self.JACKPOT
+		elif numMatching == self.NUM_WHITES:
+			amt = 1000000
+		elif red and numMatching == 4:
+			amt = 50000
+		elif numMatching == 4 or (red and numMatching == 3):
+			amt = 100
+		elif (red and numMatching == 2) or numMatching == 3:
+			amt = 7
+		elif red:
+			amt = 4
+
+		return amt
 		
