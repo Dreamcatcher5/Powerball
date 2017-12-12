656d82c25f36c54975ca2ea3f368f18b9955343f
Unknown <johnstone.ben71@gmail.com>
2017-12-11 22:42:22 -0500

    Simplified finding classes in factory

--- src/picker/factory.py
+++ src/picker/factory.py
@@ -1,5 +1,7 @@
 import inspect
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from picker import Picker
 
@@ -12,31 +14,25 @@ class PickerFactory(object):
 
 	def GetPickers(self):
 
-		# print os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 		# switch to that directory
-		curDir = os.getcwd() ### This isn't right, could be running from different directory
-		newDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-		print("Changing to ", newDir)
-		os.chdir(newDir)
-		for f in os.listdir(newDir):
-			if not os.path.isfile(os.path.join(newDir, f)) or f.find(".py") == -1 or f == "__init__.py":
+		print("Factory: populating Picker classes")
+		packageDir = os.path.dirname(os.path.abspath(__file__))
+		for f in os.listdir(packageDir):
+			if not os.path.isfile(os.path.join(packageDir, f)) or f.find(".py") == -1 or f == "__init__.py":
 				continue
 
 			# Load module (filename minus the .py)
-			print("Importing ", f)
+			print("\tImporting module ", f)
 			mod = __import__(f[:f.rindex(".py")])
 			# getmembers returns a list of tuples: (name, class)
 			classes = inspect.getmembers(mod, lambda x: inspect.isclass(x) \
 										 and issubclass(x, Picker) and x != Picker)
 			for c in classes:
+				print("\t\tAdding class ", c[0])
 				self._pickerClasses[c[0]] = c[1]
 
 
-		# switch back directories when done
-
-
-
-	def CreatePicker(self, className):
+	def CreatePicker(self, className, **kwargs):
 
 
 		if not self._pickerClasses:
@@ -48,4 +44,4 @@ class PickerFactory(object):
 		else:
 			print(self._pickerClasses)
 			print(self._pickerClasses[className])
-			return self._pickerClasses[className]()
+			return self._pickerClasses[className](**kwargs)
