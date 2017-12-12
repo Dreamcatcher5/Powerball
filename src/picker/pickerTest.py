70d47d90213eb406b78afeccc672ff4245e7bef9
Unknown <johnstone.ben71@gmail.com>
2017-12-11 22:41:04 -0500

    Expanded picker unit test

--- src/picker/pickerTest.py
+++ src/picker/pickerTest.py
@@ -1,2 +1,19 @@
-import picker
-import powerball
+from drawing import Drawing
+from factory import PickerFactory
+from randomPicker import RandomPicker
+
+
+if __name__ == "__main__":
+    d = {"picksPerDrawing": 5, "altReds": False}
+
+    # Test creating pickers
+
+
+    # Test factory
+    pf = PickerFactory()
+    print(pf)
+    rp = pf.CreatePicker("RandomPicker", **d)
+    print(rp)
+    rp.Pick(None)
+    for draw in rp.GetPicks():
+        print(draw)
